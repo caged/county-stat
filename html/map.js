3bece759e88c3dccd65d6063bd67de2e70304d60
Justin Palmer <justin@github.com>
2015-05-25 08:53:53 -0700

    use value-by-alpha-style map technique

--- html/map.js
+++ html/map.js
@@ -5,20 +5,23 @@ d3.select('.js-hamburger').on('click', function() {
 })
 
 var el     = d3.select('.js-canvas')
-    margin = { top: 20, right: 0, bottom: 0, left: 0 },
+    margin = { top: 0, right: 0, bottom: 0, left: 0 },
     width  = parseFloat(el.style('width')) - margin.left - margin.right,
     height = parseFloat(el.style('height')) - margin.top - margin.bottom
 
 var projection = d3.geo.albersUsa()
-  .scale(width)
+  .scale(width + (width / 4))
   .translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
   .projection(projection)
 
 var color = d3.scale.linear()
-    .range(["#2B3034", "#F5EA96"])
+    .range(['#ccc','#22303E'])
     .interpolate(d3.interpolateHcl)
+
+var alpha = d3.scale.linear()
+  .range([0.2, 1])
 
 var vis = d3.select('.js-canvas').append('svg')
   .attr('width', width + margin.left + margin.right)
@@ -26,16 +29,17 @@ var vis = d3.select('.js-canvas').append('svg')
 .append('g')
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
+var properties = d3.select('.js-properties').append('div')
+
 d3.json('/us-10m.json', function(us) {
   var land = topojson.feature(us, us.objects.land),
       states = topojson.mesh(us, us.objects.states, function(a, b) { return a.id !== b.id; }),
-      mesh = topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); }),
       counties = topojson.feature(us, us.objects.counties).features
 
   var table = el.attr('data-table') || 'x00_counts',
       column = el.attr('data-column') || 'b00002e1'
 
-  function format(row) { row.value = +row.value; return row }
+  function format(row) { row.per_100k = +row.per_100k; return row }
   d3.csv('/csv/' + table + '/' + column + '.csv', format, function(data) {
 
     counties.forEach(function(c) {
@@ -43,12 +47,15 @@ d3.json('/us-10m.json', function(us) {
           county = data.filter(function(d) { return d.id == c.id })
 
       if(county.length == 1) {
-        c.properties.value = county[0].value
+        c.properties = county[0]
         c.properties.name  = county[0].name
       }
     })
 
-    color.domain(d3.extent(data, function(d) { return d.value }))
+    color.domain(d3.extent(data, function(d) { return d.per_100k }))
+    alpha.domain(d3.extent(data, function(d) { return d.population }))
+
+    scatter(data, color, alpha)
 
     vis.append('path')
       .datum(land)
@@ -60,15 +67,19 @@ d3.json('/us-10m.json', function(us) {
     .selectAll('path')
       .data(counties)
     .enter().append('path')
-      .style("fill", function(d) { return color(d.properties.value) })
+      .style("fill", function fillthis(d) { return color(d.properties.per_100k) })
+      .style('fill-opacity', function alphathis(d) { return alpha(d.properties.population) })
       .attr('d', path)
-      .on('click', function(d) { console.log(d.properties); })
+      .on('mouseover', function(d) {
+        var str = '<table>',
+            keys = Object.keys(d.properties)
+        keys.forEach(function(k) {
+          str += '<tr><td>' + k + '</td><td>' + d.properties[k] + '</td></tr>'
+        })
 
-
-    vis.append('path')
-      .datum(mesh)
-      .attr('class', 'county-mesh')
-      .attr('d', path)
+        str += '</table>'
+        properties.html(str)
+      })
 
     vis.append('path')
       .datum(states)
@@ -76,3 +87,7 @@ d3.json('/us-10m.json', function(us) {
       .attr('d', path)
   })
 })
+
+function scatter(data, color, alpha) {
+
+}
