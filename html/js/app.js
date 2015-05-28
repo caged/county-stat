c878b3eb8feb6b52ac5daa1690ffe578d33ae0b4
Justin Palmer <justin@github.com>
2015-05-27 20:49:54 -0700

    hookup pushstate

--- html/js/app.js
+++ html/js/app.js
@@ -2,14 +2,16 @@
 
 (function() {
 
-  var el = d3.select('.js-container'),
-      table = el.attr('data-table'),
-      column = el.attr('data-column')
+  let container = d3.select('.js-container'),
+      table  = container.attr('data-table'),
+      column = container.attr('data-column'),
+      desc   = container.attr('data-desc')
+
 
   // Load map data
   d3.json('/us-10m.json', function load(err, data) {
     cstat.dispatch.geodata(err, data)
-    loadMetricsForTableAndColumn(table, column)
+    loadMetricsForTableAndColumn(table, column, desc)
   })
 
   // Sidebar expanding
@@ -19,21 +21,35 @@
     })
   })
 
+  d3.select(window).on('popstate', function() {
+    let state = d3.event.state,
+        table = state.table,
+        column = state.column,
+        desc   = state.desc
+
+    loadMetricsForTableAndColumn(table, column, desc)
+  })
+
   // Change stats
   d3.selectAll('.js-stat-link').on('click', function() {
     d3.event.preventDefault()
 
     let el = d3.select(this),
         table = el.attr('data-table'),
-        column = el.attr('data-column')
+        column = el.attr('data-column'),
+        desc   = el.attr('title')
 
-    loadMetricsForTableAndColumn(table, column)
+    let state = { table: table, column: column, desc: desc }
+    history.pushState(state, "page 2", '/' + table + '/' + column)
+
+    d3.selectAll('.js-stat-link').classed('selected', false)
+    el.classed('selected', true)
+    loadMetricsForTableAndColumn(table, column, desc)
   })
 
-  function loadMetricsForTableAndColumn(table, column) {
-    d3.csv('/csv/' + table + '/' + column + '.csv', function(data) {
-      cstat.dispatch.statchange(data)
+  function loadMetricsForTableAndColumn(table, column, desc) {
     d3.csv('/csv/' + table + '/' + column + '.csv', typeify, function(data) {
+      cstat.dispatch.statchange(data, table, column, desc)
     })
   }
 
