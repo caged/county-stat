0a5b5807ceeeecb9c9155e462bfc77a377c51dea
Justin Palmer <justin@github.com>
2015-05-30 11:23:37 -0700

    add a few clustering methods

---
+++ html/js/cluster.js
@@ -0,0 +1,31 @@
+'use strict';
+
+(function() {
+  let cluster = cstat.cluster = {}
+
+  cluster.quantize = function(data, bins) {
+    return d3.scale.quantize().range(bins)
+  }
+
+  cluster.jenks = function(data, bins) {
+    data = data.sort(function(a, b) { return a - b })
+    return ss.jenks(data, bins || 9)
+  }
+
+  cluster.headtail = function(data) {
+    data = data.sort(function(a, b) { return a - b })
+    let min = d3.min(data),
+        mean = d3.mean(data),
+        bins = [min]
+
+    while(data.length > 0) {
+      data = data.filter(function(d) { return d > mean })
+      if(data.length > 0) {
+        mean = d3.mean(data)
+        bins.push(mean)
+      }
+    }
+
+    return bins
+  }
+})()
